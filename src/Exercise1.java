aa3902f0125acbbbe7aff55a48e9ef8a503e25e9
MikeDetroit <mikedetroit@gmail.com>
2018-04-24 12:57:07 -0400

    Exercise 1 added

---
+++ src/Exercise1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Exercise1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		 Scanner scnr = new Scanner(System.in);
+	     String enterText;
+		
+	     System.out.println("Enter some text");
+	     enterText = scnr.nextLine();
+	     
+	     System.out.println(enterText);
+	}
+
+}
